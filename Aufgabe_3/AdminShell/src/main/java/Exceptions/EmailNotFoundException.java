226fb72f98f1859c547247ff6cb4755234e0746f
mcmyffin <dimitri.meier@haw-haburg.de>
2016-05-24 15:36:52 +0200

    delete project

--- Aufgabe_3/AdminShell/src/main/java/Exceptions/EmailNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package Exceptions;
-
-/**
- * Created by dima on 23.05.16.
- */
-public class EmailNotFoundException extends Exception{
-
-
-}
--- Aufgabe_3/AdminShell/src/main/java/Exceptions/TimeServiceException.java
+++
@@ -1,9 +0,0 @@
-package Exceptions;
-
-/**
- * Created by dima on 23.05.16.
- */
-public class TimeServiceException extends Exception{
-
-
-}
